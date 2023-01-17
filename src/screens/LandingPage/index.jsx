a5ac1c20d0e0c939cae53a35a149e7387d8ce9bb
psuedo97 <santosh.work965@gmail.com>
2023-01-17 20:03:16 +0530

    tex added on Landing page.

--- src/screens/LandingPage/index.jsx
+++ src/screens/LandingPage/index.jsx
@@ -6,14 +6,17 @@ import "../../style/index.css";
 const LandingPage = () => {
   const [dark, setDark] = useState("");
   const [light, setLight] = useState("");
+  const [textColor, setTextColor] = useState("text-light-theme");
 
   const changeTheme = () => {
     if (dark === "d-none") {
       setDark("");
       setLight("d-none");
+      setTextColor("text-dark-theme");
     } else {
       setDark("d-none");
       setLight("");
+      setTextColor("text-light-theme");
     }
   };
 
@@ -21,24 +24,36 @@ const LandingPage = () => {
     setDark("d-none");
   }, []);
   return (
-    <>
-      <div
-        className={
-          "d-flex justify-content-center align-items-center height-100 bg-light-svg " +
-          light
-        }
-      >
-        <LogoSvgLight onClick={changeTheme} />
+    <div>
+      <div className="svgDiv">
+        <div
+          className={
+            "d-flex justify-content-center align-items-center height-100 bg-light-svg " +
+            light
+          }
+        >
+          <LogoSvgLight onClick={changeTheme} />
+          <div className="textDiv">
+            <p className={`${textColor} cursor noselect`} onClick={changeTheme}>
+              Click to switch mode|
+            </p>
+          </div>
+        </div>
+        <div
+          className={
+            "d-flex justify-content-center align-items-center height-100 bg-dark-svg " +
+            dark
+          }
+        >
+          <LogoSvgDark onClick={changeTheme} />
+          <div className="textDiv">
+            <p className={`${textColor} cursor noselect`} onClick={changeTheme}>
+              Click to switch mode|
+            </p>
+          </div>
+        </div>
       </div>
-      <div
-        className={
-          "d-flex justify-content-center align-items-center height-100 bg-dark-svg " +
-          dark
-        }
-      >
-        <LogoSvgDark onClick={changeTheme} />
-      </div>
-    </>
+    </div>
   );
 };
 
