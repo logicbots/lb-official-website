e546162575ee8d6e1fa33423cf04f1f8a44cb4cf
psuedo97 <santosh.work965@gmail.com>
2023-03-13 04:12:03 +0530

    about us page added and animation is added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import LandingPage from './screens/LandingPage';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/" element={<AboutUs/>}/>
       </Routes>
     </Router>
   );
---
+++ src/components/ThemeSwitch/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const ThemeSwitch = () => {
+  return (
+    <span class="switcher switcher-1">
+      <input type="checkbox" id="switcher-1" />
+      <label for="switcher-1"></label>
+    </span>
+  );
+};
+
+export default ThemeSwitch;
